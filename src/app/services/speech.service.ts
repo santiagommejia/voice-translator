1a6e154e38b042c3af8d2f37fe2d366d4b463ac1
Santiago Mejia <smejia@conxiuminc.com>
2024-02-03 23:03:30 -0400

    android automatic control

--- src/app/services/speech.service.ts
+++ src/app/services/speech.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { SpeechRecognition } from "@capacitor-community/speech-recognition";
+import { Platform } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
@@ -8,21 +9,23 @@ export class SpeechService {
 
   recognizedSpeech: EventEmitter<string> = new EventEmitter();
 
-  constructor() { }
+  constructor(public platform: Platform) { }
 
   public speechRecognitionAvailable: Promise<{ available: boolean }> = SpeechRecognition.available();
 
   public startSpeechRecognition(language: string) {
     SpeechRecognition.start({ language, partialResults: true, popup: false });
     SpeechRecognition.addListener('partialResults', (data: any) => {
-      if (data.matches && data.matches.length > 0) {
-        this.recognizedSpeech.emit(data.matches[0]);
-      }
-      // Android has different result type
-      // if(data.value && data.value.length > 0) {
-      //   const text = data.value[0];
-      //   console.log("partialResults was fired", text);
-      // }
+			if (this.platform.is('ios')) {
+				if (data.matches && data.matches.length > 0) {
+					this.recognizedSpeech.emit(data.matches[0]);
+				}
+			} else if (this.platform.is('android')) {
+				if(data.value && data.value.length > 0) {
+					const text = data.value[0];
+					this.recognizedSpeech.emit(text);
+				}
+			}
     });
   }
 
