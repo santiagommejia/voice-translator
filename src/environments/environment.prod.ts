1d510731e5e51ae9d78f804be85b866c0b4526fc
Santiago <smejia@conxiuminc.com>
2024-07-03 13:44:53 -0400

    removed public keys
    removed public keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  openAikey: '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',
-  googleTranslateApiKey: 'AIzaSyA2OWX9FWibBqwQcTcycG_5uqdb039p_Gs'
+  openAikey: 'your-open-api-key-here',
+  googleTranslateApiKey: 'your-google-translate-api-key-here'
 };
