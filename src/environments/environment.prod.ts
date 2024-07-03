51836b508f8971dae44e3d1d92b411e0530d8fc5
Santiago <smejia@conxiuminc.com>
2024-07-03 13:41:58 -0400

    migration commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  openAikey: 'your-openai-key-here',
-  googleTranslateApiKey: 'your-google-translate-key-here'
+  openAikey: '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',
+  googleTranslateApiKey: 'AIzaSyA2OWX9FWibBqwQcTcycG_5uqdb039p_Gs'
 };
